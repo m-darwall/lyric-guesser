737f48f959296cf35bd07ce606174b9b8eb29f85
Martin <mdotdarwall@gmail.com>
2026-05-29 19:18:53 +0100

    reinstate randomness

--- lyric-handler.js
+++ lyric-handler.js
@@ -46,7 +46,6 @@ async function getSongData (jsonPath) {
 }
 
 function chooseRandomSong (songData) {
-    return songData.songs[1];
     return songData.songs[Math.floor(Math.random() * songData.songs.length)];
 }
 
