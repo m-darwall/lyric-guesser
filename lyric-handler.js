af20aea407f794ae74c43e1ecbcdb1f326d03e7a
Martin <mdotdarwall@gmail.com>
2026-05-29 16:17:22 +0100

    added functioning guessing

--- lyric-handler.js
+++ lyric-handler.js
@@ -1,3 +1,38 @@
+let theSong = 0;
+
+class Song{
+    constructor(json, elements){
+        this.json = json;
+        this.title = json.title;
+        this.artist = json.artist;
+        this.lyrics = json.lyrics;
+        this.elements = elements;
+        this.guesses = [];
+        this.plainlyrics = json.lyrics;
+        for(let i = 0; i < json.lyrics.length; i++){
+            this.plainlyrics[i] = this.plainlyrics[i].toLowerCase();
+            this.plainlyrics[i] = this.plainlyrics[i].replace(/[.,\/#!$%^&*';:{}=\-_`~()]/g, '');
+        }
+    }
+    checkGuess(guess){
+        guess = guess.toLowerCase().replace(/[.,\/#!$%^&*';:{}=\-_`~()]/g, '');
+        let result = this.plainlyrics.includes(guess);
+        if(result && !this.guesses.includes(guess)){
+            this.guesses.push(guess);
+            document.getElementById('guess-box').value = '';
+            let indices = [];
+            for(let i = 0; i < this.plainlyrics.length; i++){
+                if (this.plainlyrics[i] === guess){
+                    indices.push(i);
+                }
+            }
+            for (let i = 0; i < indices.length; i++){
+                this.elements[indices[i]].style.color = 'white';
+            }
+        }
+    }
+}
+
 async function getSongData (jsonPath) {
     const data = await fetch(jsonPath);
     if (!data.ok) throw new Error(data.statusText);
@@ -9,8 +44,7 @@ function chooseRandomSong (songData) {
 }
 
 function displayLyrics (songData) {
-    console.log(songData.Lyrics);
-    let lyrics = songData.Lyrics;
+    let lyrics = songData.lyrics;
     let divs = []
     for (let i = 0; i < lyrics.length; i++) {
         let div = document.createElement("div");
@@ -18,9 +52,16 @@ function displayLyrics (songData) {
         divs.push(div);
         document.getElementById("lyrics-container").appendChild(div);
     }
+    return divs;
 }
 
 async function setupSong (jsonPath) {
     let song = await getSongData(jsonPath);
-    displayLyrics(song);
+    let divs = displayLyrics(song);
+    theSong = new Song(song, divs);
+}
+
+function updateGuess(song){
+    let guess = document.getElementById("guess-box").value;
+    theSong.checkGuess(guess);
 }
